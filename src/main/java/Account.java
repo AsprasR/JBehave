f86a757ad25c2371feab5a5af2e87b2e54755c62
nightspire <nightspire@DESKTOP>
2019-01-11 03:25:56 +0100

    Init JBehave project

---
+++ src/main/java/Account.java
@@ -0,0 +1,22 @@
+public class Account {
+
+    private int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int getMoney(int amount) {
+        if (balance < amount)
+            return 0;
+
+        balance = balance - amount;
+
+        return amount;
+
+    }
+}
